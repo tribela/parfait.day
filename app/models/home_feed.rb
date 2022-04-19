7456b8b85e71f8fa0e87917dc281885caaf6b4de
Jeong Arm <kjwonmail@gmail.com>
2022-04-19 22:42:39 +0900

    [Qdon] Port infinite home timeline
    Also optimise redis query

--- app/models/home_feed.rb
+++ app/models/home_feed.rb
@@ -9,4 +9,48 @@ class HomeFeed < Feed
   def regenerating?
     redis.exists?("account:#{@account.id}:regeneration")
   end
+
+  def get(limit, max_id = nil, since_id = nil, min_id = nil)
+    limit    = limit.to_i
+    max_id   = max_id.to_i if max_id.present?
+    since_id = since_id.to_i if since_id.present?
+    min_id   = min_id.to_i if min_id.present?
+
+    if min_id.present?
+      redis_min_id = fetch_min_redis_id
+      return from_redis(limit, max_id, since_id, min_id) if redis_min_id && min_id >= redis_min_id
+
+      from_database(limit, max_id, since_id, min_id)
+    else
+      statuses = from_redis(limit, max_id, since_id, min_id)
+      return statuses if statuses.size >= limit
+
+      if since_id.present?
+        redis_min_id = fetch_min_redis_id
+        return statuses if redis_min_id.present? && since_id >= redis_min_id
+      end
+
+      remaining_limit = limit - statuses.size
+      max_id = statuses.last.id unless statuses.empty?
+      statuses + from_database(remaining_limit, max_id, since_id, min_id)
+    end
+  end
+
+  protected
+
+  def from_database(limit, max_id, since_id, min_id)
+    # Note that this query will not contains direct messages
+    Status
+      .where(account: [@account] + @account.following)
+      .where(visibility: [:public, :unlisted, :private])
+      .to_a_paginated_by_id(limit, min_id: min_id, max_id: max_id, since_id: since_id)
+      .reject { |status| FeedManager.instance.filter?(:home, status, @account) }
+      .sort_by { |status| -status.id }
+  end
+
+  private
+
+  def fetch_min_redis_id
+    redis.zrangebyscore(key, '(0', '(+inf', limit: [0, 1]).first&.to_i
+  end
 end
