78330902358d0706d619ee7bf8429dcb73aec336
Jeong Arm <kjwonmail@gmail.com>
2022-08-04 20:45:44 +0900

    [Qdon] Support TagFollow on infinite home timeline

--- app/models/home_feed.rb
+++ app/models/home_feed.rb
@@ -39,14 +39,22 @@ class HomeFeed < Feed
   protected
 
   def from_database(limit, max_id, since_id, min_id)
+    tag_followings = TagFollow.where(account: @account).select(:tag_id)
     scope = Status.where(account: @account.following)
-    scope = scope.left_outer_joins(:mentions)
+    scope = scope.left_outer_joins(:mentions, :tags)
     scope = scope.where(visibility: %i(public unlisted private)).or(scope.where(mentions: { account_id: @account.id })).group(Status.arel_table[:id])
     scope = scope.or(Status.where(account: @account))
-    scope
-      .to_a_paginated_by_id(limit, min_id: min_id, max_id: max_id, since_id: since_id)
-      .reject { |status| FeedManager.instance.filter?(:home, status, @account) }
-      .sort_by { |status| -status.id }
+    scope = scope.or(Status.where(tags: { id: tag_followings }).where(visibility: :public))
+    scope = scope
+            .to_a_paginated_by_id(limit, min_id: min_id, max_id: max_id, since_id: since_id)
+            .reject do |status|
+              if status.tags.where(id: tag_followings).exists?
+                FeedManager.instance.filter?(:tags, status, @account)
+              else
+                FeedManager.instance.filter?(:home, status, @account)
+              end
+            end
+    scope.sort_by { |status| -status.id }
   end
 
   private
