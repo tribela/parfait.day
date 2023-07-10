7375254d10c71d13bdfd269f0421a5b1bba9563f
Jeong Arm <kjwonmail@gmail.com>
2023-07-10 19:51:55 +0900

    [Parfait] Show seasonal division name in the Mabinogi clock

--- app/javascript/mastodon/components/mabinogi_clock.js
+++ app/javascript/mastodon/components/mabinogi_clock.js
@@ -2,6 +2,16 @@ import React from 'react';
 
 const FIX_SECONDS = -8 * 60;
 
+const DIVISIONS = [
+  '알반 에일레르',
+  '벨테인',
+  '알반 헤루인',
+  '루나사',
+  '알반 엘베드',
+  '삼하인',
+  '임볼릭',
+];
+
 const padLeft = (str, len, pad) => {
   str = String(str);
   pad = String(pad);
@@ -19,19 +29,25 @@ const getErinnTime = (ts) => {
 
   let erinnHour = Math.floor(erinnTimestamp / 3600);
   let erinnMinute = Math.floor(erinnTimestamp / 60) % 60;
-  let erinnSecond = Math.floor(erinnTimestamp % 60);
 
   erinnHour = padLeft(erinnHour, 2, '0');
   erinnMinute = padLeft(erinnMinute, 2, '0');
-  erinnSecond = padLeft(erinnSecond, 2, '0');
 
-  return `${erinnHour}:${erinnMinute}:${erinnSecond}`;
+  return `${erinnHour}:${erinnMinute}`;
+};
+
+const getDivision = (ts) => {
+  let koreanTimestamp = Math.floor(ts / 1000 + 32400);
+
+  let weekday = (Math.floor(koreanTimestamp / 86400) - 4) % 7;
+
+  return DIVISIONS[weekday];
 };
 
 export default class MabinogiClock extends React.PureComponent {
 
   state = {
-    timestamp: 0,
+    timestamp: Date.now(),
   };
 
   componentDidMount () {
@@ -46,7 +62,10 @@ export default class MabinogiClock extends React.PureComponent {
     let { timestamp } = this.state;
     return (
       <div className='mabinogi-clock'>
-        <div className='mabinogi-clock__time'>{ getErinnTime(timestamp) }</div>
+        <div className='mabinogi-clock__time'>
+          <span>{ getDivision(timestamp) }</span>
+          <span>{ getErinnTime(timestamp) }</span>
+        </div>
       </div>
     );
   }
