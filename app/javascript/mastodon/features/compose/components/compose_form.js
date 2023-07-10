145abdf6db290f2dc85e9973b4fb13663d8b4190
Jeong Arm <kjwonmail@gmail.com>
2023-07-10 18:56:45 +0900

    [Parfait] Add mabinogi clock

--- app/javascript/mastodon/features/compose/components/compose_form.js
+++ app/javascript/mastodon/features/compose/components/compose_form.js
@@ -20,6 +20,7 @@ import ImmutablePureComponent from 'react-immutable-pure-component';
 import { length } from 'stringz';
 import { countableText } from '../util/counter';
 import Icon from 'mastodon/components/icon';
+import MabinogiClock from 'mastodon/components/mabinogi_clock';
 
 const allowedAroundShortCode = '><\u0085\u0020\u00a0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u3000\u2028\u2029\u0009\u000a\u000b\u000c\u000d';
 
@@ -285,6 +286,7 @@ class ComposeForm extends ImmutablePureComponent {
         </div>
 
         <div className='compose-form__publish'>
+          <MabinogiClock />
           <div className='compose-form__publish-button-wrapper'>
             <Button
               type='submit'
