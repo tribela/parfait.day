f9f42bb41630eb4587660396ab8b0ba84907831c
Essem <smswessem@gmail.com>
2024-06-16 14:30:22 -0500

    Disable reactions in detailed status view when visibleReactions is 0

--- app/javascript/flavours/glitch/features/status/components/detailed_status.tsx
+++ app/javascript/flavours/glitch/features/status/components/detailed_status.tsx
@@ -30,6 +30,7 @@ import { DisplayName } from '../../../components/display_name';
 import MediaGallery from '../../../components/media_gallery';
 import StatusContent from '../../../components/status_content';
 import StatusReactions from '../../../components/status_reactions';
+import { visibleReactions } from '../../../initial_state';
 import Audio from '../../audio';
 import scheduleIdleTask from '../../ui/util/schedule_idle_task';
 import Video from '../../video';
@@ -409,13 +410,15 @@ export const DetailedStatus: React.FC<{
           {...(statusContentProps as any)}
         />
 
-        <StatusReactions
-          statusId={status.get('id')}
-          reactions={status.get('reactions')}
-          addReaction={onReactionAdd}
-          removeReaction={onReactionRemove}
-          canReact={signedIn}
-        />
+        {visibleReactions && visibleReactions > 0 && (
+          <StatusReactions
+            statusId={status.get('id')}
+            reactions={status.get('reactions')}
+            addReaction={onReactionAdd}
+            removeReaction={onReactionRemove}
+            canReact={signedIn}
+          />
+        )}
 
         <div className='detailed-status__meta'>
           <div className='detailed-status__meta__line'>
