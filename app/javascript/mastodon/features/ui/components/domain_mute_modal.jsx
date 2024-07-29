44774b10233ba6ff02972838e267a521d662f0a5
Jeong Arm <kjwonmail@gmail.com>
2024-07-29 23:24:33 +0900

    [Qdon] Renew domain mute modal

--- app/javascript/mastodon/features/ui/components/domain_mute_modal.jsx
+++ app/javascript/mastodon/features/ui/components/domain_mute_modal.jsx
@@ -1,105 +1,108 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import { useCallback, useState } from 'react';
 
-import { injectIntl, FormattedMessage } from 'react-intl';
+import { defineMessages, useIntl, FormattedMessage } from 'react-intl';
 
-import { connect } from 'react-redux';
+import classNames from 'classnames';
 
-import Toggle from 'react-toggle';
+import { useDispatch } from 'react-redux';
 
-import { muteDomain, toggleHideFromHome } from '../../../actions/domain_mutes';
-import { closeModal } from '../../../actions/modal';
-import { Button } from '../../../components/button';
+import HomeIcon from '@/material-icons/400-24px/home.svg?react';
+import VisibilityOffIcon from '@/material-icons/400-24px/visibility_off.svg?react';
+import VolumeOffIcon from '@/material-icons/400-24px/volume_off.svg?react';
+import { muteDomain } from 'mastodon/actions/domain_mutes';
+import { closeModal } from 'mastodon/actions/modal';
+import { Button } from 'mastodon/components/button';
+import { CheckBox } from 'mastodon/components/check_box';
+import { Icon } from 'mastodon/components/icon';
 
-const mapStateToProps = state => {
-  return {
-    domain: state.getIn(['domain_mutes', 'new', 'domain']),
-    hideFromHome: state.getIn(['domain_mutes', 'new', 'hide_from_home']),
-  };
-};
+const messages = defineMessages({
+  hideFromHome: { id: 'domain_mute_modal.hide_from_home', defaultMessage: 'Also hide from home timeline unless you followed them' },
+  confirm: { id: 'confirmations.domain_mute.confirm', defaultMessage: 'Mute domain' },
+});
 
-const mapDispatchToProps = dispatch => {
-  return {
-    onConfirm(domain, hideFromHome) {
-      dispatch(muteDomain(domain, hideFromHome));
-    },
+export const DomainMuteModal = ({ domain }) => {
+  const intl = useIntl();
+  const dispatch = useDispatch();
+  const [hideFromHome, setHideFromHome] = useState(false);
+  const [expanded, setExpanded] = useState(false);
 
-    onClose() {
-      dispatch(closeModal({
-        modalType: undefined,
-        ignoreFocus: false,
-      }));
-    },
+  const handleClick = useCallback(() => {
+    dispatch(closeModal({ modalType: undefined, ignoreFocus: false }));
+    dispatch(muteDomain(domain, hideFromHome));
+  }, [dispatch, domain, hideFromHome]);
 
-    onToggleHideFromHome() {
-      dispatch(toggleHideFromHome());
-    },
-  };
-};
+  const handleCancel = useCallback(() => {
+    dispatch(closeModal({ modalType: undefined, ignoreFocus: false }));
+  }, [dispatch]);
 
-class DomainMuteModal extends React.PureComponent {
+  const handleToggleHideFromHome = useCallback(({ target }) => {
+    setHideFromHome(target.checked);
+  }, [setHideFromHome]);
 
-  static propTypes = {
-    domain: PropTypes.string.isRequired,
-    hideFromHome: PropTypes.bool.isRequired,
-    onClose: PropTypes.func.isRequired,
-    onConfirm: PropTypes.func.isRequired,
-    onToggleHideFromHome: PropTypes.func.isRequired,
-    intl: PropTypes.object.isRequired,
-  };
+  const handleToggleSettings = useCallback(() => {
+    setExpanded(!expanded);
+  }, [expanded, setExpanded]);
 
-  handleClick = () => {
-    this.props.onClose();
-    this.props.onConfirm(this.props.domain, this.props.hideFromHome);
-  };
+  return (
+    <div className='modal-root__modal safety-action-modal'>
+      <div className='safety-action-modal__top'>
+        <div className='safety-action-modal__header'>
+          <div className='safety-action-modal__header__icon'>
+            <Icon icon={VolumeOffIcon} />
+          </div>
 
-  handleCancel = () => {
-    this.props.onClose();
-  };
-
-  toggleHideFromHome = () => {
-    this.props.onToggleHideFromHome();
-  };
-
-  render () {
-    const { domain, hideFromHome } = this.props;
-
-    return (
-      <div className='modal-root__modal mute-modal'>
-        <div className='mute-modal__container'>
-          <p>
-            <FormattedMessage
-              id='confirmations.domain_mute.message'
-              defaultMessage='Are you sure you want to mute entire domain {domain}?'
-              values={{ domain: <strong>{domain}</strong> }}
-            />
-          </p>
-          <p className='mute-modal__explanation'>
-            <FormattedMessage
-              id='confirmations.domain_mute.explanation'
-              defaultMessage='This will hide posts from {domain} from public timeline except you followed'
-            />
-          </p>
-          <div className='setting-toggle'>
-            <Toggle id='domain-mute-modal__hide-from-home-checkbox' checked={hideFromHome} onChange={this.toggleHideFromHome} />
-            <label className='setting-toggle__label' htmlFor='domain-mute-modal__hide-from-home-checkbox'>
-              <FormattedMessage id='domain_mute_modal.hide_from_home' defaultMessage='Hide from home timeline?' />
-            </label>
+          <div>
+            <h1><FormattedMessage id='domain_mute_modal.title' defaultMessage='Mute domain?' /></h1>
+            <div>{domain}</div>
           </div>
         </div>
 
-        <div className='mute-modal__action-bar'>
-          <Button onClick={this.handleCancel} className='mute-modal__cancel-button'>
+        <div className='safety-action-modal__bullet-points'>
+          <div>
+            <div className='safety-action-modal__bullet-points__icon'><Icon icon={VisibilityOffIcon} /></div>
+            <div><FormattedMessage id='domain_mute_modal.you_wont_see_posts' defaultMessage='You will not see their posts on public timelines.' /></div>
+          </div>
+        </div>
+
+        <div className='safety-action-modal__bullet-points'>
+          <div>
+            <div className='safety-action-modal__bullet-points__icon'><Icon icon={HomeIcon} /></div>
+            <div><FormattedMessage id='domain_mute_modal.still_on_home' defaultMessage='You still see their posts from home timeline.' /></div>
+          </div>
+        </div>
+      </div>
+
+      <div className={classNames('safety-action-modal__bottom', { active: expanded })}>
+        <div className='safety-action-modal__bottom__collapsible'>
+
+          <div className='safety-action-modal__field-group'>
+            <CheckBox label={intl.formatMessage(messages.hideFromHome)} checked={hideFromHome} onChange={handleToggleHideFromHome} />
+          </div>
+        </div>
+
+        <div className='safety-action-modal__actions'>
+          <button onClick={handleToggleSettings} className='link-button'>
+            {expanded ? <FormattedMessage id='mute_modal.hide_options' defaultMessage='Hide options' /> : <FormattedMessage id='mute_modal.show_options' defaultMessage='Show options' />}
+          </button>
+
+          <div className='spacer' />
+
+          <button onClick={handleCancel} className='link-button'>
             <FormattedMessage id='confirmation_modal.cancel' defaultMessage='Cancel' />
-          </Button>
-          <Button onClick={this.handleClick}>
+          </button>
+
+          <Button onClick={handleClick}>
             <FormattedMessage id='confirmations.domain_mute.confirm' defaultMessage='Mute domain' />
           </Button>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+};
 
-}
+DomainMuteModal.propTypes = {
+  domain: PropTypes.string.isRequired,
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(injectIntl(DomainMuteModal));
+export default DomainMuteModal;
